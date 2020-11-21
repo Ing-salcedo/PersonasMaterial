a7dcc6ae7d3c1155faecb537a236f0db8c49b030
Gerardo.SalcedoP7 <ingenierosalcedo@outlook.com>
2020-11-21 11:17:01 -0500

    Agregar y listar funcionando local

--- app/src/main/java/com/example/personasmaterial/CrearPersonas.java
+++ app/src/main/java/com/example/personasmaterial/CrearPersonas.java
@@ -2,8 +2,11 @@ package com.example.personasmaterial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -26,12 +29,16 @@ public class CrearPersonas extends AppCompatActivity {
     public void guardar (View v){
         String ced, nom, apell, id;
         Persona p;
+        InputMethodManager imp;
+
         ced = cedula.getText().toString();
         nom = nombre.getText().toString();
         apell = apellido.getText().toString();
+        imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
         p = new Persona(ced,nom,apell,"");
         p.guardar();
+        imp.hideSoftInputFromWindow(cedula.getWindowToken(),0);
         limpiar();
         Snackbar.make(v,R.string.mensaje_guardado_exitosamente,Snackbar.LENGTH_LONG).show();
     }
@@ -44,5 +51,11 @@ public class CrearPersonas extends AppCompatActivity {
         nombre.setText("");
         apellido.setText("");
         cedula.requestFocus();
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(CrearPersonas.this, MainActivity.class);
+        startActivity(i);
     }
 }
